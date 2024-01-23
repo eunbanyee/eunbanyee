9cf20608edc7f6323d6f49beafbc19da7c97934d
eunbanyee <79554819+eunbanyee@users.noreply.github.com>
2024-01-23 18:31:06 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
 
-$(window).scroll(function() {
-if($(this).scrollTop() > 500) {
-$('#ul').fadeIn();
-} else {
-$('#ul').fadeOut();
-}
-});
+    $(window).scroll(function() {
+    if($(this).scrollTop() > 500) {
+    $('#ul').fadeIn();
+    } else {
+    $('#ul').fadeOut();
+    }
+    });
