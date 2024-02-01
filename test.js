119fcca58e329d86feb96e8423e4dcaa61f93010
eunbanyee <79554819+eunbanyee@users.noreply.github.com>
2024-02-01 19:20:05 +0900

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-
-    $(window).scroll(function() {
-    if($(this).scrollTop() > 500) {
-    $('#ul').fadeIn();
-    } else {
-    $('#ul').fadeOut();
-    }
-    });
